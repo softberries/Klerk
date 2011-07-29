e4ca93c7deabe770de904dc16cb54b174a63b852
softberries <krzysztof.grajek@googlemail.com>
2011-07-29 17:26:39 +0200

    added address dao

---
+++ com.softberries.klerk.dao/src/com/softberries/klerk/dao/AddressDao.java
@@ -0,0 +1,149 @@
+package com.softberries.klerk.dao;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import com.softberries.klerk.dao.to.Address;
+
+public class AddressDao extends GenericDao<Address>{
+	
+	private static final String SQL_INSERT_ADDRESS = "INSERT INTO ADDRESS(country, city, street, postCode, houseNumber, flatNumber, notes, main, person_id, company_id) VALUES(?, ?, ?, ?, ?, ?)";
+	private static final String SQL_DELETE_ADDRESS = "DELETE FROM ADDRESS WHERE id = ?";
+	private static final String SQL_FIND_ADDRESS_BY_ID = "SELECT * FROM ADDRESS WHERE id = ?";
+	private static final String SQL_DELETE_ALL_ADDRESSES = "DELETE FROM ADDRESS WHERE id > 0";
+	private static final String SQL_FIND_ADDRESS_ALL = "SELECT * FROM ADDRESS";
+	private static final String SQL_UPDATE_ADDRESS = "UPDATE ADDRESS SET country = ?, city = ?, street = ?, postCode = ?, houseNumber = ?, flatNumber = ?, notes = ?, main = ?, person_id = ?, company_id = ? WHERE id = ?";
+	
+	@Override
+	public List<Address> findAll() throws SQLException {
+		List<Address> companies = new ArrayList<Address>();
+		try{
+			init();
+			ResultSetHandler<List<Address>> h = new BeanListHandler<Address>(Address.class);
+			companies = run.query(conn, SQL_FIND_ADDRESS_ALL, h); 
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally{
+			close(conn, st, generatedKeys);
+		}
+		return companies;
+	}
+
+	@Override
+	public Address find(Long id) throws SQLException {
+		Address p = null;
+		try{
+			init();
+			ResultSetHandler<Address> h = new BeanHandler<Address>(Address.class);
+			p = run.query(conn, SQL_FIND_ADDRESS_BY_ID, h, id); 
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally{
+			close(conn, st, generatedKeys);
+		}
+		return p;
+	}
+
+	@Override
+	public void create(Address c) throws SQLException {
+		try {
+			init();
+	        st = conn.prepareStatement(SQL_INSERT_ADDRESS, Statement.RETURN_GENERATED_KEYS); 
+	        st.setString(1, c.getCountry());
+	        st.setString(2, c.getCity());
+	        st.setString(3, c.getStreet());
+	        st.setString(4, c.getPostCode());
+	        st.setString(5, c.getHouseNumber());
+	        st.setString(6, c.getFlatNumber());
+	        st.setString(7, c.getNotes());
+	        st.setBoolean(8, c.isMain());
+	        st.setLong(9, c.getPerson_id());
+	        st.setLong(10, c.getCompany_id());
+	        // run the query
+	        int i = st.executeUpdate();    
+	        System.out.println("i: " + i);
+	        if (i == -1) {
+	            System.out.println("db error : " + SQL_INSERT_ADDRESS);
+	        }
+	        generatedKeys = st.getGeneratedKeys();
+	        if (generatedKeys.next()) {
+	            c.setId(generatedKeys.getLong(1));
+	        } else {
+	            throw new SQLException("Creating user failed, no generated key obtained.");
+	        }
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally{
+			close(conn, st, generatedKeys);
+		}
+	}
+
+	@Override
+	public void update(Address c) throws SQLException {
+		try {
+			init();
+			st = conn.prepareStatement(SQL_UPDATE_ADDRESS); 
+			st.setString(1, c.getCountry());
+	        st.setString(2, c.getCity());
+	        st.setString(3, c.getStreet());
+	        st.setString(4, c.getPostCode());
+	        st.setString(5, c.getHouseNumber());
+	        st.setString(6, c.getFlatNumber());
+	        st.setString(7, c.getNotes());
+	        st.setBoolean(8, c.isMain());
+	        st.setLong(9, c.getPerson_id());
+	        st.setLong(10, c.getCompany_id());
+	        st.setLong(7, c.getId());
+	        // run the query
+	        int i = st.executeUpdate();    
+	        System.out.println("i: " + i);
+	        if (i == -1) {
+	            System.out.println("db error : " + SQL_UPDATE_ADDRESS);
+	        }
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally{
+			close(conn, st, generatedKeys);
+		}
+	}
+
+	@Override
+	public void delete(Long id) throws SQLException {
+		try {
+			init();
+			st = conn.prepareStatement(SQL_DELETE_ADDRESS); 
+	        st.setLong(1, id);
+	        // run the query
+	        int i = st.executeUpdate();    
+	        System.out.println("i: " + i);
+	        if (i == -1) {
+	            System.out.println("db error : " + SQL_DELETE_ADDRESS);
+	        }
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally{
+			close(conn, st, generatedKeys);
+		}
+	}
+
+	@Override
+	public void deleteAll() throws SQLException {
+		try{
+			init();
+			st = conn.prepareStatement(SQL_DELETE_ALL_ADDRESSES);
+			int i = st.executeUpdate();
+			System.out.println("i: " + i);
+	        if (i == -1) {
+	            System.out.println("db error : " + SQL_DELETE_ALL_ADDRESSES);
+	        }
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}finally{
+			close(conn, st, generatedKeys);
+		}
+	}
+}
--- com.softberries.klerk.dao/src/com/softberries/klerk/dao/GenericDao.java
+++ com.softberries.klerk.dao/src/com/softberries/klerk/dao/GenericDao.java
@@ -37,13 +37,25 @@ public abstract class GenericDao<T> {
 	 */
 	protected QueryRunner run = null;
 	
+	/**
+	 * Initializes connection to the database
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	public void init() throws ClassNotFoundException, SQLException{
 		run = new QueryRunner();
 		Class.forName("org.h2.Driver");
 		conn = DriverManager.getConnection("jdbc:h2:~/.klerk/klerk", "sa", "");
-//		conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
 	}
-	
+	/**
+	 * Closes all resources after execution
+	 * 
+	 * @param con {@code Connection} object
+	 * @param stm {@code PreparedStatement} object
+	 * @param gk {@code ResultSet} object
+	 * @throws SQLException
+	 */
 	protected void close(Connection con, PreparedStatement stm,
 			ResultSet gk) throws SQLException {
 		if(gk != null){
@@ -56,10 +68,43 @@ public abstract class GenericDao<T> {
 			con.close();
 		}
 	}
+	/**
+	 * Finds all records and convert it to the specific type
+	 * 
+	 * @return {@code List<T>} list of specific objects created from {@code ResultSet}
+	 *  
+	 * @throws SQLException
+	 */
 	public abstract List<T> findAll()  throws SQLException;
+	/**
+	 * Finds object of type <T> in the database
+	 * 
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 */
 	public abstract T find(Long id) throws SQLException;
+	/**
+	 * Adds new record to the database based on the Object of type T
+	 * @param t
+	 * @throws SQLException
+	 */
 	public abstract void create(T t) throws SQLException;
+	/**
+	 * Updates single record in the database
+	 * @param t
+	 * @throws SQLException
+	 */
 	public abstract void update(T t) throws SQLException;
+	/**
+	 * Deletes single record in the database
+	 * @param id
+	 * @throws SQLException
+	 */
 	public abstract void delete(Long id) throws SQLException;
+	/**
+	 * Deletes all records in a table corresponding the type T
+	 * @throws SQLException
+	 */
 	public abstract void deleteAll() throws SQLException;
 }
