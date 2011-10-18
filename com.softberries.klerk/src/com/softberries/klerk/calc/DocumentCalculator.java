360f5bbd76fd044c39ad2141570dd0ad63427bd0
softberries <krzysztof.grajek@googlemail.com>
2011-10-18 15:35:47 +0200

    added licence details and copyright notice

--- com.softberries.klerk/src/com/softberries/klerk/calc/DocumentCalculator.java
+++ com.softberries.klerk/src/com/softberries/klerk/calc/DocumentCalculator.java
@@ -1,13 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Softberries Krzysztof Grajek.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Softberries Krzysztof Grajek - initial API and implementation
+ ******************************************************************************/
 package com.softberries.klerk.calc;
 
 import java.math.BigDecimal;
-import java.net.NetPermission;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 import com.softberries.klerk.dao.to.Document;
 import com.softberries.klerk.dao.to.DocumentItem;
 import com.softberries.klerk.dao.to.VatLevelItem;
